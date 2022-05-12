0d7107666e3d0e0ffb3ecc508d7f11375e3bc6a8
eun98bi <103350895+eun98bi@users.noreply.github.com>
2022-05-12 18:21:01 +0900

    Add files via upload

---
+++ assets/js/javascript.js
@@ -0,0 +1,70 @@
+function doDisplay1(){
+	var con = document.getElementById("c1");
+    var tit = document.getElementById("t1");
+	if(con.style.display=='none'){
+		con.style.display = 'block';
+        tit.style.backgroundColor = 'white';
+        tit.style.color = 'black';
+	}else{
+		con.style.display = 'none';
+        tit.style.backgroundColor = '';
+        tit.style.color = 'white';
+	}
+}
+
+function doDisplay2(){
+	var con = document.getElementById("c2");
+    var tit = document.getElementById("t2");
+	if(con.style.display=='none'){
+		con.style.display = 'block';
+        tit.style.backgroundColor = 'white';
+        tit.style.color = 'black';
+	}else{
+		con.style.display = 'none';
+        tit.style.backgroundColor = '';
+        tit.style.color = 'white';
+	}
+}
+
+function doDisplay3(){
+	var con = document.getElementById("c3");
+    var tit = document.getElementById("t3");
+	if(con.style.display=='none'){
+		con.style.display = 'block';
+        tit.style.backgroundColor = 'white';
+        tit.style.color = 'black';
+	}else{
+		con.style.display = 'none';
+        tit.style.backgroundColor = '';
+        tit.style.color = 'white';
+	}
+}
+
+function doDisplay4(){
+	var con = document.getElementById("c4");
+    var tit = document.getElementById("t4");
+	if(con.style.display=='none'){
+		con.style.display = 'block';
+        tit.style.backgroundColor = 'white';
+        tit.style.color = 'black';
+	}else{
+		con.style.display = 'none';
+        tit.style.backgroundColor = '';
+        tit.style.color = 'white';
+	}
+}
+
+function doDisplay5(){
+	var con = document.getElementById("c5");
+    var tit = document.getElementById("t5");
+	if(con.style.display=='none'){
+		con.style.display = 'block';
+        tit.style.backgroundColor = 'white';
+        tit.style.color = 'black';
+	}else{
+		con.style.display = 'none';
+        tit.style.backgroundColor = '';
+        tit.style.color = 'white';
+	}
+}
+
